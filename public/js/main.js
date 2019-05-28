ece23ef4b7a5155326d4542cdda7ddb5be3a798e
benrugman <benrugman@yahoo.co.uk>
2019-05-28 15:53:39 +0100

    completed skills and portfolio sections

--- public/js/main.js
+++ public/js/main.js
@@ -37,4 +37,13 @@ toggler.addEventListener("click", e => {
   overlayNav.classList.toggle("open");
 });
 
+window.onscroll = function() {
+  var nav = document.querySelector("nav");
+  if (window.pageYOffset >= 100) {
+    nav.classList.add("navbar1");
+  } else {
+    nav.classList.remove("navbar1");
+  }
+};
+
 // this.classList.toggle("open"));
