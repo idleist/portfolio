8abb12d405ca3f6847f35f2a40cbe7c964453ac1
benrugman <benrugman@yahoo.co.uk>
2019-06-06 19:16:00 +0100

    added hover effects on elements, added scroll behaviour to links, completed responsive navbar, added cv download functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,19 @@
-//     $("nav a[href*='#']").on("click", function() {
-//       $("html, body").animate(
-//         {
-//           scrollTop: $($(this).attr("href")).offset().top - 100
-//         },
-//         1500
-//       );
-//     });
-
-//     $("#up").on("click", function() {
-//       $("html, body").animate(
-//         {
-//           scrollTop: 0
-//         },
-//         1500
-//       );
-//     });
-
 const toggler = document.querySelector(".menu-toggler");
 const bars = [...document.querySelectorAll(".bar")];
 const overlayNav = document.querySelector(".overlay-nav");
+const overlayNavLinks = [...document.querySelectorAll(".nav-link")];
 
-toggler.addEventListener("click", e => {
+function openNav() {
+  console.log("click");
   toggler.classList.toggle("open");
   bars.forEach(bar => bar.classList.toggle("open"));
   overlayNav.classList.toggle("open");
+}
+toggler.addEventListener("click", openNav);
+
+overlayNavLinks.forEach(navlink => {
+  console.log(navlink);
+  navlink.addEventListener("click", openNav);
 });
 
 window.onscroll = function() {
